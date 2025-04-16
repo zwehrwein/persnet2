c6d86ec90d173d8b0237cc4f94f00ef625777daf
zwehrwein <wehrwein@protonmail.com>
2025-04-16 04:31:29 -0400

    added question on providing generic support

--- R/proportion_alters_provide_support.R
+++ R/proportion_alters_provide_support.R
@@ -1,11 +1,11 @@
 #' Calculate alters who provide some kind of support to ego
 #'
-#' Computes the proportion of alters who live more than 15 miles away from the ego in a persnet dataframe row.
-#' This is done by summing the proportions of alters categorized as "16_50miles" and "more50miles", and rounding the result to two decimal places.
+#' Proportion of alters ego answered yes to "which person or persons support you most often?
+#' You can choose more than one person."
 #'
 #' @param persnet_row A single row of a persnet dataframe.
 #'
-#' @return A numeric value representing the proportion of alters living more than 15 miles away.
+#' @return A numeric value representing the proportion of alters who "support ego msot often"
 #' @export
 #'
 proportion_alters_provide_support <- function(persnet_row){
